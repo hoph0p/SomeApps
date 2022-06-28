73c739f283de7c7a9cff80fe4cb5aa710ff39de2
Hop_Hop <novikov.hop.work@yandex.ru>
2022-06-28 15:20:52 +0300

    some changes

--- djangoProject/urls.py
+++ djangoProject/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('actors/', include('actors.urls'))
+    path('actors/', include('actors.urls')),
+    path('guinness/', include('guinness.urls')),
 ]
--- guinness/views.py
+++ guinness/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
 
+
 # Create your views here.
 def get_guinness_world_records(request):
-    return render(request, 'guinness/guinnesworldrecords.html')
+    data = {
+        'power_man': 'Narve Laeret',
+        'bar_name': 'Bob`s BBQ & Grill',
+        'count_needle': 1790,
+    }
+    return render(request, 'guinness/guinnessworldrecords.html', context=data)
